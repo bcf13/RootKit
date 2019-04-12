06640e20da95957f1d22cdedb1761e273879adda
Benjamin Flowers <bcf13@duke.edu>
2019-04-11 21:54:05 -0400

    added test script

--- homework5-kit/.tmp_versions/sneaky_mod.mod
+++ homework5-kit/.tmp_versions/sneaky_mod.mod
@@ -1,3 +1,2 @@
 /home/bcf13/HW/HW5/homework5-kit/sneaky_mod.ko
 /home/bcf13/HW/HW5/homework5-kit/sneaky_mod.o
-
---
+++ searchScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ls ./homework5-kit
+
+find ~/HW -name sneaky_process 
+
+ls /proc
+
+ps -a -u bcf13
